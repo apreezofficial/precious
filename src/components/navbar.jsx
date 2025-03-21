584d5cac22e1e83e6d42d635dbc50c149a8013dc
Precious Adedokun <apreezofficial@gmail.com>
2025-03-21 11:36:42 +0100

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,21 @@
-import React from 'react',
-import MusicPlayer from 'MusicPlayer'
-import './style.css'
-function Alink({text, link}) {
-   return (
-    <a href={link}>{text}</a>
-   )
+import React from 'react';
+import MusicPlayer from './MusicPlayer'; // Ensure correct path
+import './style.css';
+
+function Alink({ text, link }) {
+  return <a href={link}>{text}</a>;
 }
-export default function Navbar(){
-    return (
-<>
-   <MusicPlayer />
-<div className="navbar">
-    <div className='logotext'>Precious! ⚡</div>
-    <a href="https://github.com/apreezofficial" className='git' target='_blank'><i class="fab fa-github"></i> github</a>
-</div>
-</>
-    );
+
+export default function Navbar() {
+  return (
+    <>
+      <MusicPlayer />
+      <div className="navbar">
+        <div className="logotext">Precious! ⚡</div>
+        <a href="https://github.com/apreezofficial" className="git" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i> GitHub
+        </a>
+      </div>
+    </>
+  );
 }
