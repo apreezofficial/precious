b82c4c705e3fcb251d5958fb1a69b57f3c2c4c75
Precious Adedokun <apreezofficial@gmail.com>
2025-03-21 11:54:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css';
 import Navbar from './components/navbar'
 import Page from './components/page'
-import MusicPlayer from './components/MusicPlayer'
 function App() {
   return (
      <>
-       <MusicPlayer />
      <Navbar />
      <Page />
      </>   
