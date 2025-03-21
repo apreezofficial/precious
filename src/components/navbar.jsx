81fb7ad72cca46d0401281948afa0eb00e378253
Precious Adedokun <apreezofficial@gmail.com>
2025-03-21 11:41:56 +0100

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import MusicPlayer from './MusicPlayer'; // Ensure correct path
 import './style.css';
+
 export default function Navbar() {
   return (
-    <>
+    <React.Fragment>
       <MusicPlayer />
-      <div className="navbar">
+      <nav className="navbar">
         <div className="logotext">Precious! ⚡</div>
         <a href="https://github.com/apreezofficial" className="git" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-github"></i> GitHub
         </a>
-      </div>
-    </>
+      </nav>
+    </React.Fragment>
   );
 }
