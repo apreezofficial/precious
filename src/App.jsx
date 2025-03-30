48d117399f0d89bb117196fbb9e774498f25508d
Precious Adedokun <apreezofficial@gmail.com>
2025-03-30 13:35:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
+import SEO from "./seo";
+import FAQSchema from "./FAQSchema";
 import Navbar from './components/navbar'
 import Page from './components/page'
 function App() {
   return (
      <>
+       <SEO />
+      <FAQSchema />
      <Navbar />
      <Page />
      </>   
